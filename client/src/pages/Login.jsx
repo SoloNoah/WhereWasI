0422a891a5ae0dd22f5548fd94e22082849cf150
SoloNoah <39374814+SoloNoah@users.noreply.github.com>
2022-03-15 16:43:36 +0200

    login/register + form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+import Form from '../components/Form/Form';
+
+const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (window.localStorage.getItem('accessToken')) {
+      props.setLoggedin(true);
+    }
+  }, []);
+  const onClick = async () => {
+    const newUser = { email, password };
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const body = JSON.stringify(newUser);
+      navigate('/');
+      // const res = await axios.post('/api/auth', body, config);
+      // if (res.status === 200) {
+      //   props.setLoggedin(true);
+      //   window.localStorage.setItem('accessToken', res.data.token);
+      //   navigate('/');
+      //   return;
+      // }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className='full-page'>
+      <Form
+        setTop={setEmail}
+        setBottom={setPassword}
+        onClick={onClick}
+        botType={'password'}
+        inputTop={'Email'}
+        inputBot={'Password'}
+        title={'Login'}
+        subTitle={'Helping you binge with ease'}
+        next={'register'}
+      />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Form from '../components/Form/Form';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+  const onClick = async () => {
+    const newUser = { email, password };
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      navigate('/login');
+
+      // const body = JSON.stringify(newUser);
+      // const res = await axios.post("/api/users/", body, config);
+      // if (res.status === 200) {
+      //   window.localStorage.setItem("accessToken", res.data.token);
+      //   // history.replace("/login");
+      //   navigate('/login');
+
+      //   return;
+      // }
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  };
+
+  return (
+    <div className='full-page'>
+      <Form
+        setTop={setEmail}
+        setBottom={setPassword}
+        botType={'password'}
+        onClick={onClick}
+        inputTop={'Email'}
+        inputBot={'Password'}
+        title={'Register'}
+        subTitle={'Join and start binging'}
+        next={'login'}
+      />
+    </div>
+  );
 };
 
 export default Register;
