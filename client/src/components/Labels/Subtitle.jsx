09aefe946bb397703d8700e69d28639e1c13583b
SoloNoah <39374814+SoloNoah@users.noreply.github.com>
2022-03-23 15:17:03 +0200

    fixed passing props to styled

--- client/src/components/Labels/Subtitle.jsx
+++ client/src/components/Labels/Subtitle.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const SubTitle = styled.p`
-  font-size: ${(lg) => (lg ? "20px" : "55px")};
+  font-size: ${(props) => (props.lg ? "25px" : "20px")};
   text-align: center;
   color: #fff;
   font-weight: bold;
 `;
 
-const Subtitle = ({ subtitle }) => {
+const Subtitle = ({ subtitle, lg }) => {
+  console.log(lg);
+  if (lg) {
+    return <SubTitle lg>{subtitle}</SubTitle>;
+  }
   return <SubTitle>{subtitle}</SubTitle>;
 };
 
